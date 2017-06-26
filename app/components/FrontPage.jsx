0c396c09c1babd2ec54135d4944f71ea2504552c
colleenlavin <cml388@mun.ca>
2017-06-26 17:56:00 -0400

    Charts and graphs exist

--- app/components/FrontPage.jsx
+++ app/components/FrontPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { VictoryPie, VictoryChart, VictoryScatter, VictoryLine, VictoryBar } from 'victory';
+//import PieChart from './PieChart'
 
 export default class Home extends Component {
     constructor() {
@@ -8,13 +10,57 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div className= "home">
+            <div className="home">
 
-                <div className= "text">
+                <div className="text">
                     <h1>Spending Habits</h1>
                 </div>
-
+                <div className="chart col-md-4">
+                    <h2>Breakdown</h2>
+                    <VictoryPie />
+                </div>
+                <div className="chart col-md-4">
+                    <h2>Savings Progress</h2>
+                    <VictoryChart height={450}>
+                        <VictoryScatter
+                            style={{ data: { fill: "purple" } }}
+                            symbol="square"
+                            size={5}
+                            data={[
+                                { x: -4, y: -4 },
+                                { x: 4, y: 2, fill: "red" },
+                                { x: 1.8, y: 3 }
+                            ]} />
+                        <VictoryLine
+                            y={(data) => data.x} />
+                        <VictoryBar
+                            style={{ data: { fill: "orange" } }}
+                            data={[
+                                { x: 2, y: -3 },
+                                { x: -3, y: 4 },
+                                { x: 1, y: 3 }
+                            ]} />
+                    </VictoryChart>
+                </div>
+                {/*<div>
+                    <PieChart
+                        labels
+                        data={[
+                            { key: 'A', value: 100, color: '#aaac84' },
+                            { key: 'B', value: 200, color: '#dce7c5' },
+                            { key: 'C', value: 50, color: '#e3a51a' }
+                        ]}
+                        styles={{
+                            '.chart_text': {
+                                fontSize: '1em',
+                                fill: '#fff'
+                            }
+                        }}
+                    />*/}
+                {/*<PieChart />*/}
             </div>
+
+
         )
     }
 
